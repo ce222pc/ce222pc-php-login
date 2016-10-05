e7c7b0b8b2c74e286c04b93db3880857710738f2
Christoffer Ekberg <ce222pc@student.lnu.se>
2016-10-05 16:40:25 +0200

    Remove unneeded database code

--- db.php
+++ db.php
@@ -6,27 +6,5 @@ class Database {
     function __construct() {
         $this->db = new PDO("sqlite:./database/database.sqlite3");
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        if(!$this->databaseExists()) {
-            $this->$db->exec("CREATE TABLE user (
-                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                name VARCHAR(255) UNIQUE NOT NULL,
-                hash VARCHAR(255) NOT NULL,
-                cookie_password VARCHAR(255)
-            )"
-        );
-
-            $user = array(":name" => "Admin", ":hash" => password_hash("Password", PASSWORD_DEFAULT));
-            $insertUser = $db->prepare("INSERT INTO user(name, hash) values(:name, :hash)");
-            $insertUser->execute($user);
-        }
-    }
-
-    private function databaseExists() {
-        try {
-            return $this->db->query("SELECT 1 FROM user");
-        } catch(Exception $e) {
-
-        }
     }
 }
